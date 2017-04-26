76d8025a09db5605464aa0bf7adf3a514a635933
John Talio <johntalio@yahoo.com>
2017-04-26 11:03:54 -0400

    Fix model - am able to grab data from api

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,7 +1,7 @@
 class Doctor < ApplicationRecord
 
-  ny_doctors = HTTParty.get "https://api.betterdoctor.com/2016-03-01/doctors?location=NY&user_location=40.7128%2C74.0059&skip=0&limit=10&user_key=9b916adf7c966d9dce5bb16db9f4b2b2"
-  ny_doctors.each do |key, value|
-    self.create!(image_url: doctor[:data][v][:profile])
+  ny_doctors_hash = HTTParty.get "https://api.betterdoctor.com/2016-03-01/doctors?location=NY&user_location=40.7128%2C74.0059&skip=0&limit=10&user_key=9b916adf7c966d9dce5bb16db9f4b2b2"
+  ny_doctors_hash["data"].each do |doctor|
+    self.find_or_create_by(image_url: doctor["profile"]["image_url"], first_name: doctor["profile"]["first_name"], last_name: doctor["profile"]["last_name"], title: doctor["profile"]["title"], bio: doctor["profile"]["bio"])
   end
 end
