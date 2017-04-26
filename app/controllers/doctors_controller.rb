d449570c5cbe23cf5fa5920ab4b6b8c1b8bc34e1
John Talio <johntalio@yahoo.com>
2017-04-26 13:13:30 -0400

    Index path with search params

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,12 @@
 class DoctorsController < ApplicationController
+
+  def index
+    @doctors = Doctor.all
+    if params[:search]
+      @doctors = Doctor.search(params[:search]).order("created_at DESC")
+    else
+      @doctors = Doctor.all.order("created_at DESC")
+    end
+  end
+
 end
