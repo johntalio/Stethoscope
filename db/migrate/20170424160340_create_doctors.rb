f5d54157455ea906854c80a9dac0ba8abd16ffe4
John Talio <johntalio@yahoo.com>
2017-04-26 14:18:34 -0400

    Add null: false

--- db/migrate/20170424160340_create_doctors.rb
+++ db/migrate/20170424160340_create_doctors.rb
@@ -2,9 +2,9 @@ class CreateDoctors < ActiveRecord::Migration[5.0]
   def change
     create_table :doctors do |t|
       t.string :image_url
-      t.string :first_name
-      t.string :last_name
-      t.string :title
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :title, null: false
       t.string :bio
 
       t.timestamps
