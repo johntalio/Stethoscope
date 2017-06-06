78b8182dbb16dd55768fb851dc3419bd560e4a5e
John Talio <johntalio@yahoo.com>
2017-06-06 10:41:16 -0400

    Add length validation to password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, :email, :password, presence: true
   validates :email, uniqueness: true
+  validates :password, length: {in: 6..20}
   has_secure_password
 end
