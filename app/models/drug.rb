6c916ad2de2b83e9bd477d77b430688aefe0a8ef
John Talio <johntalio@yahoo.com>
2017-06-16 15:44:47 -0400

    Add validations

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -1,4 +1,10 @@
 class Drug < ApplicationRecord
   has_many :prescriptions
   has_many :users, :through => :prescriptions
+
+  validates :generic_name, :brand_name, presence: true
+
+  def self.search(search)
+    where("generic_name LIKE ? OR brand_name LIKE ?", "%#{search}%", "%#{search}%")
+  end
 end
