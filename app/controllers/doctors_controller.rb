baedcdb56e80097091b42b2b823a873cc2439948
John Talio <johntalio@yahoo.com>
2017-06-20 12:52:29 -0400

    Fix for uniformity

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -6,10 +6,9 @@ class DoctorsController < ApplicationController
   def index
     @doctors = Doctor.all
     if params[:search]
-      @doctors = Doctor.search(params[:search]).order("created_at DESC")
+      @doctors = Doctor.search(params[:search].upcase_first).order("created_at DESC")
     else
       @doctors = Doctor.all.order("created_at DESC")
     end
   end
-
 end
